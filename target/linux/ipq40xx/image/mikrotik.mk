d3dc3d481937558a820c136b1474ad4c94c6b3d8
Robert Marko <robimarko@gmail.com>
2021-11-30 10:25:52 +0100

    ipq-wifi: remove packaged BDF-s for MikroTik devices
    Since we now provide the BDF-s for MikroTik IPQ40xx devices on the fly,
    there is noneed to include package and ship them like we do now.
    
    This also resolves the performance issues that happen as MikroTik
    changes the boards and ships them under the same revision but they
    actually ship with and require a different BDF.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/ipq40xx/image/mikrotik.mk
+++ target/linux/ipq40xx/image/mikrotik.mk
@@ -24,8 +24,7 @@ define Device/mikrotik_hap-ac2
 	$(call Device/mikrotik_nor)
 	DEVICE_MODEL := hAP ac2
 	SOC := qcom-ipq4018
-	DEVICE_PACKAGES := ipq-wifi-mikrotik_hap-ac2 -kmod-ath10k-ct \
-		kmod-ath10k-ct-smallbuffers
+	DEVICE_PACKAGES := -kmod-ath10k-ct kmod-ath10k-ct-smallbuffers
 endef
 TARGET_DEVICES += mikrotik_hap-ac2
 
@@ -36,7 +35,7 @@ define Device/mikrotik_hap-ac3
 	BLOCKSIZE := 128k
 	PAGESIZE := 2048
 	KERNEL_UBIFS_OPTS = -m $$(PAGESIZE) -e 124KiB -c $$(PAGESIZE) -x none
-	DEVICE_PACKAGES := kmod-ledtrig-gpio ipq-wifi-mikrotik_hap-ac3
+	DEVICE_PACKAGES := kmod-ledtrig-gpio
 endef
 TARGET_DEVICES += mikrotik_hap-ac3
 
@@ -44,6 +43,6 @@ define Device/mikrotik_sxtsq-5-ac
 	$(call Device/mikrotik_nor)
 	DEVICE_MODEL := SXTsq 5 ac (RBSXTsqG-5acD)
 	SOC := qcom-ipq4018
-	DEVICE_PACKAGES := ipq-wifi-mikrotik_sxtsq-5-ac rssileds
+	DEVICE_PACKAGES := rssileds
 endef
 TARGET_DEVICES += mikrotik_sxtsq-5-ac
